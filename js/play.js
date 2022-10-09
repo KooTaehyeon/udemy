caab565ca4cc06404b740509a1fe46208586721f
Kth <vcro12123@naver.com>
2022-10-09 17:26:25 +0900

    let const 와 화살표 함수

--- js/play.js
+++ js/play.js
@@ -2,14 +2,18 @@ let name = 'Max';
 let age = 29;
 let hasHobbies = true;
 
-function summarzeUser(userName, userAge, userHashobbies) {
+age = 30;
+const summarzeUser = (userName, userAge, userHashobbies) => {
   return (
     'Name is' +
     userName +
-    ', age is' +
+    ', age is ' +
     userAge +
     ', Hashobbies is ' +
     userHashobbies
   );
-}
+};
+const add = (a) => a + 3;
+
+console.log(add(2));
 console.log(summarzeUser(name, age, hasHobbies));
