d204def27f8299e0c443350220fbd701b8653af4
Kth <vcro12123@naver.com>
2022-10-10 16:10:00 +0900

    응답 전송

--- node/app.js
+++ node/app.js
@@ -4,5 +4,11 @@ const http = require('http');
 const server = http.createServer((req, res) => {
   console.log(req.url, req.method, req.headers);
   //process.exit();  //서버 종료
+  res.setHeader('content-Type', 'text/html');
+  res.write('<html>');
+  res.write('<head><title>My First Page</title></head>');
+  res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+  res.write('</html>');
+  res.end();
 });
 server.listen(3003);
