5313c71bc07d7c139e03b01bd438cc9099deee68
Kth <vcro12123@naver.com>
2022-10-10 19:43:19 +0900

    블로킹 및 논 블로킹 코드

--- node/app.js
+++ node/app.js
@@ -1,13 +1,12 @@
 const http = require('http');
 const fs = require('fs');
 
-// 서버에 요청이 들어올떄마다 node.js 를 호출하는 콜백함수
 const server = http.createServer((req, res) => {
   const url = req.url;
   const method = req.method;
   if (url === '/') {
     res.write('<html>');
-    res.write('<head><title>Enter Message</title></head>');
+    res.write('<head><title>Enter Message</title><head>');
     res.write(
       '<body><form action="/message" method="POST"><input type="text" name="message"><button type="submit">Send</button></form></body>'
     );
@@ -15,18 +14,28 @@ const server = http.createServer((req, res) => {
     return res.end();
   }
   if (url === '/message' && method === 'POST') {
-    fs.writeFileSync('message.txt', 'DUMMY');
-    res.statusCode = 302;
-    res.setHeader('Location', '/');
-    return res.end();
+    const body = [];
+    req.on('data', (chunk) => {
+      console.log(chunk);
+      body.push(chunk);
+    });
+    return req.on('end', () => {
+      const parsedBody = Buffer.concat(body).toString();
+      const message = parsedBody.split('=')[1];
+      fs.writeFile('message.txt', message, (err) => {
+        res.statusCode = 302;
+        res.setHeader('Location', '/');
+        console.log(err);
+        return res.end();
+      });
+    });
   }
-  console.log(req.url, req.method, req.headers);
-  //process.exit();  //서버 종료
-  res.setHeader('content-Type', 'text/html');
+  res.setHeader('Content-Type', 'text/html');
   res.write('<html>');
-  res.write('<head><title>My First Page</title></head>');
-  res.write('<body><h1>Hello from my Node.js Server</h1></body>');
+  res.write('<head><title>My First Page</title><head>');
+  res.write('<body><h1>Hello from my Node.js Server!</h1></body>');
   res.write('</html>');
   res.end();
 });
-server.listen(3003);
+
+server.listen(3004);
