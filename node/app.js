80bb99a6e3517fd46feaaf61d53a55c48577daff
Kth <vcro12123@naver.com>
2022-10-30 16:54:11 +0900

    path

--- node/app.js
+++ node/app.js
@@ -3,16 +3,19 @@
 const express = require('express');
 const bodyParser = require('body-parser');
 const app = express();
+const path = require('path');
 
 const adminRoutes = require('./routes/admin');
 const shopRoutes = require('./routes/shop');
-app.use(bodyParser.urlencoded({ extended: false }));
 
-app.use(adminRoutes);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use('/admin', adminRoutes);
 app.use(shopRoutes);
 
 app.use((req, res, next) => {
-  res.status(404).send('<h1>Page not Found</h1>');
+  res.status(404).sendFile(path.join(__dirname, 'views', '404.html'));
 });
 
 //const server = http.createServer(app);
--- node/routes/admin.js
+++ node/routes/admin.js
@@ -1,14 +1,12 @@
 const express = require('express');
-
+const path = require('path');
 const router = express.Router();
-
-router.get('/app-product', (req, res, next) => {
-  res.send(
-    '<form action="/product" method="POST"><input type="text" name="title"/><button type="submit">Add Product</button></form>'
-  );
+const rootDir = require('../util/path');
+router.get('/add-product', (req, res, next) => {
+  res.sendFile(path.join(rootDir, 'views', 'add-product.html'));
 });
 
-router.post('/product', (req, res, next) => {
+router.post('/add-product', (req, res, next) => {
   console.log(req.body);
   res.redirect('/');
 });
