214fd481ad8401223bed26caa06459835f99825d
Kth <vcro12123@naver.com>
2022-10-30 18:49:45 +0900

    pug start

--- node/routes/shop.js
+++ node/routes/shop.js
@@ -1,9 +1,11 @@
 const express = require('express');
 const path = require('path');
-const router = express.Router();
 const rootDir = require('../util/path');
+const adminData = require('./admin');
+const router = express.Router();
 router.get('/', (req, res, next) => {
-  res.sendFile(path.join(rootDir, 'views', 'shop.html'));
+  res.render('shop');
+  // res.sendFile(path.join(rootDir, 'views', 'shop.html'));
 });
 
 module.exports = router;
