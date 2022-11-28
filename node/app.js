4b518c0e820b9595c2eaab91a10776635451ec49
Kth <vcro12123@naver.com>
2022-11-28 19:58:10 +0900

    udemy

--- node/app.js
+++ node/app.js
@@ -9,6 +9,8 @@ const Product = require('./models/product');
 const User = require('./models/user');
 const Cart = require('./models/cart');
 const CartItem = require('./models/cart-item');
+const Order = require('./models/order');
+const OrderItem = require('./models/order-item');
 
 const app = express();
 
@@ -41,10 +43,13 @@ User.hasOne(Cart);
 Cart.belongsTo(User);
 Cart.belongsToMany(Product, { through: CartItem });
 Product.belongsToMany(Cart, { through: CartItem });
+Order.belongsTo(User);
+User.hasMany(Order);
+Order.belongsToMany(O);
 
 sequelize
-  //.sync()
-  .sync({ force: true })
+  .sync()
+  //.sync({ force: true })
   .then((result) => {
     return User.findByPk(1);
     // console.log(result);
@@ -53,10 +58,13 @@ sequelize
     if (!user) {
       return User.create({ name: 'Max', email: 'test@test.com' });
     }
-    return Promise.resolve(user);
+    return user;
   })
   .then((user) => {
     //console.log(user);
+    return user.createCart();
+  })
+  .then((cart) => {
     app.listen(3000);
   })
   .catch((err) => {
